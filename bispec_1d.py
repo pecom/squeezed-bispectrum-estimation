9ee51a8c35fdb51b13b33168b5d7ba7a5b1403c9
Prak <8682372+pecom@users.noreply.github.com>
2021-08-16 17:49:13 -0400

    mask and basic mask estimate

--- bispec_1d.py
+++ bispec_1d.py
@@ -5,12 +5,17 @@ import scipy.signal as signal
 
 class SampleLine:
     
-    def __init__(self, N, power=-1, amp=-1):
+    def __init__(self, N, periodic=False, power=-1, amp=-1):
         self.N = N
         rng = np.random.default_rng()
+        periodic = int(N/2)
+        self.dsize = int(N/2 + 1)
         self.rng = rng
         
-        self.full_data = rng.standard_normal(N)
+        if periodic:
+            smaldat = rng.standard_normal(periodic)
+            fdat = np.concatenate((smaldat, smaldat[::-1]))
+            self.full_data = fdat
         
         if power==-1:
             self.ps_power = rng.uniform()*3 + 1
@@ -20,20 +25,23 @@ class SampleLine:
             self.ps_amp = rng.uniform()*5
         else:
             self.ps_amp = amp
-        self.kn, self.ps = self.create_ps(self.ps_power, self.ps_amp, N)
+        
+        self.kn, self.ps = self.create_ps(self.ps_power, self.ps_amp, self.N)
         self.ps_sqrt = np.sqrt(self.ps)
+        self.mask = self.create_mask()
         
     def create_ps(self, power, amp, ds):
         kvec = np.fft.fftfreq(ds) * ds
         if power > 0:
             kvec[0] = np.inf
         knorm = np.abs(kvec)
+        knorm = knorm[:self.dsize]
 
         ps = amp/(knorm**power)
         return knorm, ps
     
     def create_sample_spec(self):
-        line_subset = self.rng.standard_normal((self.N, 2)).view(dtype=complex).flatten()
+        line_subset = self.rng.standard_normal((self.dsize, 2)).view(dtype=complex).flatten()
         f_spec = line_subset * self.ps_sqrt
         return f_spec
     
@@ -42,20 +50,26 @@ class SampleLine:
         ifspec = np.fft.irfft(fspec)
         return ifspec
                   
-    # Estimate power spectrum given realizations. Likely complete nonsense/garbage below:
     def basic_psest(self):
         dat = self.create_dsample(verbose=False)
         scaled_fcor = (np.abs(np.fft.rfft(dat))**2)
         return scaled_fcor
     
-    def multi_psest(self, N=1000):
-        fin_est = []
-        for i in range(N):
-            fin_est.append(self.basic_psest())
-        fin_est = np.array(fin_est)
-        meaned_fest = np.mean(fin_est, axis=0)
-        return meaned_fest
+    def masked_basic_psest(self):
+        dat = self.create_dsample(verbose=False)
+        masked_dat = dat * self.mask
+        scaled_fcor = (np.abs(np.fft.rfft(masked_dat))**2)
+        return scaled_fcor
                   
+    def create_mask(self, n_holes=2):
+        mask = np.ones(self.N)
+        rstart = np.sort(self.rng.integers(0, self.N, n_holes))
+        
+        hsize = 32
+        # For now make fixed hole size:
+        for r in rstart:
+            mask[r:r+hsize] = 0
+        return mask
         
     
  
