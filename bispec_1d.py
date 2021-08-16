ed50f4fa4c503e33e9e8004de6b30aaca4f4474b
Prak <8682372+pecom@users.noreply.github.com>
2021-08-16 14:04:03 -0400

    hopefully proper minimum data generation

--- bispec_1d.py
+++ bispec_1d.py
@@ -5,15 +5,12 @@ import scipy.signal as signal
 
 class SampleLine:
     
-    def __init__(self, N, c, power=-1, amp=-1):
+    def __init__(self, N, power=-1, amp=-1):
         self.N = N
-        self.copies = c
-        dsize = int(N*c)
         rng = np.random.default_rng()
         self.rng = rng
-        line_data = rng.standard_normal(N)
-        self.full_data = np.tile(line_data, c)
-        self.dsize = dsize
+        
+        self.full_data = rng.standard_normal(N)
         
         if power==-1:
             self.ps_power = rng.uniform()*3 + 1
@@ -36,39 +33,19 @@ class SampleLine:
         return knorm, ps
     
     def create_sample_spec(self):
-        rstart = self.rng.integers(0, self.dsize-self.N)
-        line_subset = self.full_data[rstart:rstart+self.N]
-        ff_line = np.fft.fft(line_subset)
-        f_spec = ff_line * self.ps_sqrt
+        line_subset = self.rng.standard_normal((self.N, 2)).view(dtype=complex).flatten()
+        f_spec = line_subset * self.ps_sqrt
         return f_spec
     
     def create_dsample(self, verbose=False):
         fspec = self.create_sample_spec()
-        ifspec = np.fft.ifft(fspec)
-        dset = np.real(ifspec)
-        if verbose:
-            print("Imaginary part magnitude: ", np.mean(np.abs(ifspec.imag)))
-        return dset
+        ifspec = np.fft.irfft(fspec)
+        return ifspec
                   
-        
     # Estimate power spectrum given realizations. Likely complete nonsense/garbage below:
     def basic_psest(self):
         dat = self.create_dsample(verbose=False)
-        t1 = np.fft.fft(dat)
-        good_stop = int(len(t1)/2)
-
-        corrs = signal.correlate(t1, t1)
-        corr_lags = signal.correlation_lags(len(t1), len(t1))
-        corrs = corrs/np.max(corrs)
-
-        pos_lags = corr_lags >= 0
-
-        f_corr = np.fft.fft(corrs)
-        corr_k = np.arange(int(np.sum(pos_lags)/2)).astype(float)
-        corr_k[0] = np.inf
-
-        self.corr_k = corr_k
-        scaled_fcor = np.abs(f_corr[pos_lags][:good_stop])**2/corr_k**2
+        scaled_fcor = (np.abs(np.fft.rfft(dat))**2)
         return scaled_fcor
     
     def multi_psest(self, N=1000):
--- test_bispec.py
+++ test_bispec.py
@@ -5,19 +5,7 @@ import matplotlib.pyplot as plt
 import bispec_1d
 from bispec_1d import SampleLine
 
-kpow = 2.1
 Ndata = 512
-test_line = SampleLine(Ndata, 2)
+test_line = SampleLine(Ndata)
 print(test_line.create_dsample())
 
-# meaned_fest = test_line.multi_psest(1000)
-# kn, ps = test_line.create_ps(kpow, 1, Ndata)
-
-
-# fig, ax = plt.subplots(1, figsize=(6,6))
-# ax.plot(kn, ps, label="Theory")
-# ax.plot(test_line.corr_k, meaned_fest/test_line.corr_k**kpow, label="Data")
-# ax.semilogx()
-# ax.semilogy()
-# ax.legend()
-# plt.show()
